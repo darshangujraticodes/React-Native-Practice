f6352c760ebc9a24c54f731fd72a9fa1ab726280
darshangujraticodes <darshangujrati5@gmail.com>
2025-04-23 13:15:09 +0530

    react native post required

--- SecondApp/app/components/CrudNetworking.tsx
+++ SecondApp/app/components/CrudNetworking.tsx
@@ -4,6 +4,9 @@ import {
   FlatList,
   StyleSheet,
   ActivityIndicator,
+  TextInput,
+  Pressable,
+  LogBox,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 
@@ -14,10 +17,20 @@ type PostApiDataType = {
   body: string;
 };
 
+type FormErrorType = {
+  title?: string;
+  body?: string;
+};
+
 const CrudNetworking = () => {
   const [apiPostData, setApiPostData] = useState<PostApiDataType[]>([]);
   const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
+
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [formErrors, setFormErrors] = useState<FormErrorType>({});
+  const [posting, setPosting] = useState(false);
 
   const fetchApiData = async (limit = 20) => {
     try {
@@ -56,6 +69,55 @@ const CrudNetworking = () => {
     setRefreshing(false);
   };
 
+  const validateForm = (): boolean => {
+    let formErrorList: FormErrorType = {};
+    if (!postTitle) formErrorList.title = "Title is Required!";
+    if (!postBody) formErrorList.body = "Body is Required!";
+
+    setFormErrors(formErrorList);
+
+    return Object.keys(formErrorList).length === 0;
+  };
+
+  const addPost = async () => {
+    setPosting(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        userId: Date.now(),
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+      }),
+    });
+
+    const newPost = await response.json();
+    setApiPostData([newPost, ...apiPostData]);
+  };
+
+  const handlePostSubmit = () => {
+    console.log(postTitle, postBody);
+
+    console.log(formErrors);
+
+    console.log({
+      userId: Date.now(),
+      id: Date.now(),
+      title: postTitle,
+      body: postBody,
+    });
+
+    if (validateForm()) {
+      addPost();
+      setPostTitle("");
+      setPostBody("");
+      setPosting(false);
+
+      console.log("Post Submitted Successfully !");
+    }
+  };
+
   if (loading) {
     return (
       <View style={CrudStyles.parentViewContainer}>
@@ -68,51 +130,116 @@ const CrudNetworking = () => {
   }
 
   return (
-    <View>
-      <FlatList
-        data={apiPostData}
-        keyExtractor={(item) => item.id.toString()}
-        ItemSeparatorComponent={<View style={{ height: 13 }}></View>}
-        renderItem={({ item }) => {
-          //   console.log(item);
-          return (
-            <View>
-              <Text style={CrudStyles.postTitle}>{item.title} </Text>
-              <Text style={{}}>{item.body} </Text>
+    <>
+      <View style={[CrudStyles.inputContainer]}>
+        <Text
+          style={{
+            fontSize: 25,
+            textAlign: "center",
+            marginBottom: 15,
+          }}
+        >
+          API Post Request
+        </Text>
+        <View style={{ marginBottom: 10 }}>
+          <TextInput
+            placeholder="Enter Your Post Title"
+            style={[CrudStyles.inputText]}
+            value={postTitle}
+            onChangeText={setPostTitle}
+          />
+
+          {formErrors.title ? (
+            <Text
+              style={{
+                marginTop: 5,
+                marginLeft: 10,
+                color: "red",
+                marginBottom: 5,
+              }}
+            >
+              {formErrors.title}
+            </Text>
+          ) : null}
+        </View>
+        <View style={{ marginBottom: 10 }}>
+          <TextInput
+            placeholder="Enter Your Post Body"
+            style={[CrudStyles.inputText]}
+            value={postBody}
+            onChangeText={setPostBody}
+          />
+          {formErrors.body ? (
+            <Text
+              style={{
+                marginTop: 5,
+                marginLeft: 10,
+                color: "red",
+                marginBottom: 5,
+              }}
+            >
+              {formErrors.body}
+            </Text>
+          ) : null}
+        </View>
+
+        <Pressable onPress={handlePostSubmit} disabled={posting}>
+          <Text
+            style={[
+              CrudStyles.submitBtn,
+              posting ? { opacity: 0.7 } : { opacity: 1 },
+            ]}
+          >
+            {posting ? "Adding..." : "Add Post"}
+          </Text>
+        </Pressable>
+      </View>
+      <View>
+        <FlatList
+          data={apiPostData}
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={<View style={{ height: 13 }}></View>}
+          renderItem={({ item }) => {
+            //   console.log(item);
+            return (
+              <View>
+                <Text style={CrudStyles.postTitle}>{item.title} </Text>
+                <Text style={CrudStyles.postBody}>{item.body} </Text>
+              </View>
+            );
+          }}
+          refreshing={refreshing}
+          onRefresh={handleRefresh}
+          ListEmptyComponent={
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+              <Text>No Post Data Found</Text>
             </View>
-          );
-        }}
-        refreshing={refreshing}
-        onRefresh={handleRefresh}
-        ListEmptyComponent={
-          <View style={{ justifyContent: "center", alignItems: "center" }}>
-            <Text>No Post Data Found</Text>
-          </View>
-        }
-        ListHeaderComponent={
-          <Text
-            style={{
-              fontSize: 25,
-              textAlign: "center",
-              marginBottom: 15,
-            }}
-          >
-            API Get Request of Post Info
-          </Text>
-        }
-        ListFooterComponent={
-          <Text
-            style={{
-              fontSize: 25,
-              textAlign: "center",
-              marginTop: 15,
-            }}
-          >
-            API Post Data Ends
-          </Text>
-        }
-      />
-    </View>
+          }
+          ListHeaderComponent={
+            <Text
+              style={{
+                fontSize: 25,
+                textAlign: "center",
+                marginBottom: 15,
+              }}
+            >
+              API Get Request
+            </Text>
+          }
+          ListFooterComponent={
+            <Text
+              style={{
+                fontSize: 25,
+                textAlign: "center",
+                marginTop: 15,
+              }}
+            >
+              API Post Data Ends
+            </Text>
+          }
+        />
+      </View>
+    </>
   );
 };
 
@@ -132,5 +259,25 @@ const CrudStyles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  inputContainer: {
+    marginBottom: 17,
+  },
+  inputText: {
+    borderWidth: 1,
+    borderColor: "#262626",
+    padding: 10,
+    fontSize: 15,
+    borderRadius: 8,
+    // marginBottom: 10,
+  },
+  submitBtn: {
+    textAlign: "center",
+    backgroundColor: "#6E5EF4",
+    color: "white",
+    fontSize: 22,
+    borderRadius: 8,
+    paddingVertical: 8,
   },
 });
